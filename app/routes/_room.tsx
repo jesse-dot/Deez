ed7a9dc2dc209ff57eb42fc0da0cb13a5eac5747
jesse-dot <67440987+jesse-dot@users.noreply.github.com>
2024-04-16 14:22:21 -0400

    Initial commit

---
+++ app/routes/_room.tsx
@@ -0,0 +1,122 @@
+import type { LoaderFunctionArgs } from '@remix-run/cloudflare'
+import { json } from '@remix-run/cloudflare'
+import { Outlet, useLoaderData, useParams } from '@remix-run/react'
+import { useMemo, useState } from 'react'
+import invariant from 'tiny-invariant'
+import { EnsureOnline } from '~/components/EnsureOnline'
+import { EnsurePermissions } from '~/components/EnsurePermissions'
+import { Icon } from '~/components/Icon/Icon'
+
+import { usePeerConnection } from '~/hooks/usePeerConnection'
+import usePushedTrack from '~/hooks/usePushedTrack'
+import useRoom from '~/hooks/useRoom'
+import type { RoomContextType } from '~/hooks/useRoomContext'
+import useUserMedia from '~/hooks/useUserMedia'
+
+function numberOrUndefined(value: unknown): number | undefined {
+	const num = Number(value)
+	return isNaN(num) ? undefined : num
+}
+
+export const loader = async ({ context }: LoaderFunctionArgs) => {
+	const {
+		mode,
+		TRACE_LINK,
+		API_EXTRA_PARAMS,
+		MAX_WEBCAM_FRAMERATE,
+		MAX_WEBCAM_BITRATE,
+		MAX_WEBCAM_QUALITY_LEVEL,
+	} = context
+	return json({
+		mode,
+		userDirectoryUrl: context.USER_DIRECTORY_URL,
+		traceLink: TRACE_LINK,
+		apiExtraParams: API_EXTRA_PARAMS,
+		maxWebcamFramerate: numberOrUndefined(MAX_WEBCAM_FRAMERATE),
+		maxWebcamBitrate: numberOrUndefined(MAX_WEBCAM_BITRATE),
+		maxWebcamQualityLevel: numberOrUndefined(MAX_WEBCAM_QUALITY_LEVEL),
+	})
+}
+
+export default function RoomWithPermissions() {
+	return (
+		<EnsurePermissions>
+			<EnsureOnline
+				fallback={
+					<div className="grid h-full place-items-center">
+						<div>
+							<h1 className="flex items-center gap-3 text-3xl font-black">
+								<Icon type="SignalSlashIcon" />
+								You are offline
+							</h1>
+						</div>
+					</div>
+				}
+			>
+				<Room />
+			</EnsureOnline>
+		</EnsurePermissions>
+	)
+}
+
+function Room() {
+	const [joined, setJoined] = useState(false)
+	const { roomName } = useParams()
+	invariant(roomName)
+
+	const {
+		mode,
+		userDirectoryUrl,
+		traceLink,
+		apiExtraParams,
+		maxWebcamBitrate = 1_200_000,
+		maxWebcamFramerate = 24,
+		maxWebcamQualityLevel = 1080,
+	} = useLoaderData<typeof loader>()
+
+	const userMedia = useUserMedia(mode)
+	const room = useRoom({ roomName, userMedia })
+	const { peer, debugInfo, iceConnectionState } =
+		usePeerConnection(apiExtraParams)
+
+	const scaleResolutionDownBy = useMemo(() => {
+		const videoStreamTrack = userMedia.videoStreamTrack
+		const height = videoStreamTrack?.getCapabilities().height?.max ?? 0
+		const width = videoStreamTrack?.getCapabilities().width?.max ?? 0
+		// we need to do this in case camera is in portrait mode
+		const smallestDimension = Math.min(height, width)
+		return Math.max(smallestDimension / maxWebcamQualityLevel, 1)
+	}, [maxWebcamQualityLevel, userMedia.videoStreamTrack])
+
+	const pushedVideoTrack = usePushedTrack(peer, userMedia.videoStreamTrack, {
+		maxFramerate: maxWebcamFramerate,
+		maxBitrate: maxWebcamBitrate,
+		scaleResolutionDownBy,
+	})
+	const pushedAudioTrack = usePushedTrack(peer, userMedia.audioStreamTrack, {
+		priority: 'high',
+	})
+	const pushedScreenSharingTrack = usePushedTrack(
+		peer,
+		userMedia.screenShareVideoTrack
+	)
+
+	const context: RoomContextType = {
+		joined,
+		setJoined,
+		traceLink,
+		userMedia,
+		userDirectoryUrl,
+		peer,
+		peerDebugInfo: debugInfo,
+		iceConnectionState,
+		room,
+		pushedTracks: {
+			video: pushedVideoTrack,
+			audio: pushedAudioTrack,
+			screenshare: pushedScreenSharingTrack,
+		},
+	}
+
+	return <Outlet context={context} />
+}
