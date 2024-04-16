12333fdc7a402aa9804ed28583b268e1082b0ca9
jesse-dot <67440987+jesse-dot@users.noreply.github.com>
2024-04-16 14:22:23 -0400

    Initialize deployments

---
+++ app/types/Env.ts
@@ -0,0 +1,14 @@
+export type Env = {
+	USER_DIRECTORY_URL?: string
+	FEEDBACK_URL?: string
+	FEEDBACK_QUEUE?: Queue
+	CALLS_APP_ID: string
+	CALLS_APP_SECRET: string
+	TRACE_LINK?: string
+	API_EXTRA_PARAMS?: string
+	limiters: DurableObjectNamespace
+	rooms: DurableObjectNamespace
+	MAX_WEBCAM_FRAMERATE?: string
+	MAX_WEBCAM_BITRATE?: string
+	MAX_WEBCAM_QUALITY_LEVEL?: string
+}
